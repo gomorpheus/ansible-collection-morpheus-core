12a33b12c8bdd4d7dc9729bd8af920bb079bd98c
James Riach <james@jriach.co.uk>
2023-06-25 17:27:32 +0100

    Improve module documentation

--- plugins/modules/appliance_facts.py
+++ plugins/modules/appliance_facts.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -21,160 +23,164 @@ EXAMPLES = r'''
 '''
 
 RETURN = r'''
-"ansible_facts": {
-        "gather_subset": [
-            "all"
-        ],
-        "license": {
-            "accountName": "MyCompany",
-            "amazonProductCodes": null,
-            "config": {},
-            "dateCreated": "2022-11-17T10:05:22Z",
-            "endDate": "2023-08-31T00:00:00Z",
-            "features": {
-                "activity": true,
-                "analytics": true,
-                "approvalServices": true,
-                "approvals": true,
-                "apps": true,
-                "archives": true,
-                "automation": true,
-                "automationServices": true,
-                "backupServices": true,
-                "backups": true,
-                "boot": true,
-                "buildServices": true,
-                "clouds": true,
-                "cmdbServices": true,
-                "codeService": true,
-                "cypher": true,
-                "dashboard": true,
-                "deploymentServices": true,
-                "deployments": true,
-                "discovery": true,
-                "dnsServices": true,
-                "groups": true,
-                "guidance": true,
-                "hosts": true,
-                "identityServices": true,
-                "imageBuilder": true,
-                "instances": true,
-                "ipamServices": true,
-                "keyPairs": true,
-                "library": true,
-                "loadBalancerServices": true,
-                "loadBalancers": true,
-                "logging": true,
-                "loggingServices": true,
-                "migrations": true,
-                "monitoring": true,
-                "monitoringServices": true,
-                "network": true,
-                "plans": true,
-                "pricing": true,
-                "scheduling": true,
-                "securityServices": true,
-                "serviceDiscoveryServices": true,
-                "sslCertificates": true,
-                "storage": true,
-                "templates": true,
-                "tenants": true,
-                "trustServices": true,
-                "usage": true,
-                "userGroups": true,
-                "users": true,
-                "virtualImages": true
-            },
-            "freeTrial": false,
-            "hardLimit": true,
-            "lastUpdated": "2022-11-17T10:05:22Z",
-            "maxInstances": 1000,
-            "maxMemory": 0,
-            "maxStorage": 0,
-            "multiTenant": true,
-            "productTier": "enterprise",
-            "reportStatus": true,
-            "startDate": "2022-08-22T00:00:00Z",
-            "supportLevel": "standard",
-            "whitelabel": true,
-            "zoneTypes": null
-        },
-        "module_setup": true,
-        "settings": {
-            "applianceUrl": "https://cmp.domain.tld",
-            "corsAllowed": null,
-            "currencyKey": null,
-            "currencyProvider": null,
-            "defaultRoleId": null,
-            "defaultUserRoleId": null,
-            "disableAfterAttempts": "5",
-            "disableAfterDaysInactive": null,
-            "dockerPrivilegedMode": false,
-            "enabledZoneTypes": [
-                {
-                    "id": 4,
-                    "name": "Amazon"
-                },
-                {
-                    "id": 9,
-                    "name": "Azure (Public)"
-                },
-                {
-                    "id": 11,
-                    "name": "DigitalOcean"
-                },
-                {
-                    "id": 3,
-                    "name": "Morpheus"
-                },
-                {
-                    "id": 18,
-                    "name": "Oracle Public Cloud"
-                },
-                {
-                    "id": 40,
-                    "name": "PowerVC"
-                },
-                {
-                    "id": 17,
-                    "name": "UpCloud"
-                },
-                {
-                    "id": 38,
-                    "name": "VMware Fusion"
-                },
-                {
-                    "id": 28,
-                    "name": "VMware vCenter"
-                },
-                {
-                    "id": 34,
-                    "name": "vCloud Director"
-                }
+ansible_facts:
+    description: Example of returned ansible_facts
+    returned: always
+    sample:
+    "ansible_facts": {
+            "gather_subset": [
+                "all"
             ],
-            "expirePwdDays": null,
-            "internalApplianceUrl": null,
-            "maintenanceMode": false,
-            "proxyDomain": null,
-            "proxyHost": null,
-            "proxyPassword": null,
-            "proxyPasswordHash": null,
-            "proxyPort": null,
-            "proxyUser": null,
-            "proxyWorkstation": null,
-            "registrationEnabled": false,
-            "smtpMailFrom": "morpheus@domain.tld",
-            "smtpPassword": null,
-            "smtpPasswordHash": null,
-            "smtpPort": "25",
-            "smtpSSL": false,
-            "smtpServer": "smtp.domain.tld",
-            "smtpTLS": true,
-            "smtpUser": null,
-            "statsRetainmentPeriod": null,
-            "warnUserDaysBefore": null
+            "license": {
+                "accountName": "MyCompany",
+                "amazonProductCodes": null,
+                "config": {},
+                "dateCreated": "2022-11-17T10:05:22Z",
+                "endDate": "2023-08-31T00:00:00Z",
+                "features": {
+                    "activity": true,
+                    "analytics": true,
+                    "approvalServices": true,
+                    "approvals": true,
+                    "apps": true,
+                    "archives": true,
+                    "automation": true,
+                    "automationServices": true,
+                    "backupServices": true,
+                    "backups": true,
+                    "boot": true,
+                    "buildServices": true,
+                    "clouds": true,
+                    "cmdbServices": true,
+                    "codeService": true,
+                    "cypher": true,
+                    "dashboard": true,
+                    "deploymentServices": true,
+                    "deployments": true,
+                    "discovery": true,
+                    "dnsServices": true,
+                    "groups": true,
+                    "guidance": true,
+                    "hosts": true,
+                    "identityServices": true,
+                    "imageBuilder": true,
+                    "instances": true,
+                    "ipamServices": true,
+                    "keyPairs": true,
+                    "library": true,
+                    "loadBalancerServices": true,
+                    "loadBalancers": true,
+                    "logging": true,
+                    "loggingServices": true,
+                    "migrations": true,
+                    "monitoring": true,
+                    "monitoringServices": true,
+                    "network": true,
+                    "plans": true,
+                    "pricing": true,
+                    "scheduling": true,
+                    "securityServices": true,
+                    "serviceDiscoveryServices": true,
+                    "sslCertificates": true,
+                    "storage": true,
+                    "templates": true,
+                    "tenants": true,
+                    "trustServices": true,
+                    "usage": true,
+                    "userGroups": true,
+                    "users": true,
+                    "virtualImages": true
+                },
+                "freeTrial": false,
+                "hardLimit": true,
+                "lastUpdated": "2022-11-17T10:05:22Z",
+                "maxInstances": 1000,
+                "maxMemory": 0,
+                "maxStorage": 0,
+                "multiTenant": true,
+                "productTier": "enterprise",
+                "reportStatus": true,
+                "startDate": "2022-08-22T00:00:00Z",
+                "supportLevel": "standard",
+                "whitelabel": true,
+                "zoneTypes": null
+            },
+            "module_setup": true,
+            "settings": {
+                "applianceUrl": "https://cmp.domain.tld",
+                "corsAllowed": null,
+                "currencyKey": null,
+                "currencyProvider": null,
+                "defaultRoleId": null,
+                "defaultUserRoleId": null,
+                "disableAfterAttempts": "5",
+                "disableAfterDaysInactive": null,
+                "dockerPrivilegedMode": false,
+                "enabledZoneTypes": [
+                    {
+                        "id": 4,
+                        "name": "Amazon"
+                    },
+                    {
+                        "id": 9,
+                        "name": "Azure (Public)"
+                    },
+                    {
+                        "id": 11,
+                        "name": "DigitalOcean"
+                    },
+                    {
+                        "id": 3,
+                        "name": "Morpheus"
+                    },
+                    {
+                        "id": 18,
+                        "name": "Oracle Public Cloud"
+                    },
+                    {
+                        "id": 40,
+                        "name": "PowerVC"
+                    },
+                    {
+                        "id": 17,
+                        "name": "UpCloud"
+                    },
+                    {
+                        "id": 38,
+                        "name": "VMware Fusion"
+                    },
+                    {
+                        "id": 28,
+                        "name": "VMware vCenter"
+                    },
+                    {
+                        "id": 34,
+                        "name": "vCloud Director"
+                    }
+                ],
+                "expirePwdDays": null,
+                "internalApplianceUrl": null,
+                "maintenanceMode": false,
+                "proxyDomain": null,
+                "proxyHost": null,
+                "proxyPassword": null,
+                "proxyPasswordHash": null,
+                "proxyPort": null,
+                "proxyUser": null,
+                "proxyWorkstation": null,
+                "registrationEnabled": false,
+                "smtpMailFrom": "morpheus@domain.tld",
+                "smtpPassword": null,
+                "smtpPasswordHash": null,
+                "smtpPort": "25",
+                "smtpSSL": false,
+                "smtpServer": "smtp.domain.tld",
+                "smtpTLS": true,
+                "smtpUser": null,
+                "statsRetainmentPeriod": null,
+                "warnUserDaysBefore": null
+            }
         }
-    }
 '''
 
 from ansible.module_utils.basic import AnsibleModule
