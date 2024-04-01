486f772206bec384e07f23d270904ee4fed1e1a1
James Riach <james@jriach.co.uk>
2024-04-01 22:05:05 +0100

    Add tenant module
    * Add tenant module to create, update and delete tenants

---
+++ plugins/doc_fragments/currency_option.py
@@ -0,0 +1,47 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+class ModuleDocFragment(object):
+    DOCUMENTATION = r'''
+options:
+    currency:
+        description:
+            - ISO Currency Code.
+        type: string
+        choices:
+            - AUD
+            - BGN
+            - BRL
+            - CAD
+            - CHF
+            - CLF
+            - CLP
+            - CNY
+            - CZK
+            - DKK
+            - EUR
+            - GBP
+            - HKD
+            - HRK
+            - HUF
+            - IDR
+            - ILS
+            - INR
+            - JPY
+            - KRW
+            - MXN
+            - MYR
+            - NOK
+            - NZD
+            - PHP
+            - PLN
+            - RON
+            - RUB
+            - SEK
+            - SGD
+            - THB
+            - TRY
+            - USD
+            - ZAR
+'''
